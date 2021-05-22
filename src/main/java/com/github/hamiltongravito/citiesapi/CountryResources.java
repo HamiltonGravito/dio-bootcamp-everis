57fbbe3377f1ab6a4324063692ade296120f7516
HamiltonGravito <hamiltongravito@gmail.com>
2021-05-22 18:48:00 -0300

    Criação das classes Countries e configuração MIGRATION para resolver problema com DOCKER (Win10).

---
+++ src/main/java/com/github/hamiltongravito/citiesapi/CountryResources.java
@@ -0,0 +1,23 @@
+package com.github.hamiltongravito.citiesapi;
+
+import com.github.hamiltongravito.citiesapi.countries.Country;
+import com.github.hamiltongravito.citiesapi.repository.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/countries")
+public class CountryResources {
+
+    @Autowired
+    private CountryRepository repository;
+
+    @GetMapping
+    public List<Country> countries(){
+        return repository.findAll();
+    }
+}
